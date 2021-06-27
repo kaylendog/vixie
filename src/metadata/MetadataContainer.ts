953435b280ae0e50212fcb489ae4c1e6c14a2778
SkyezerFox <actuallyori@gmail.com>
2021-06-27 15:10:01 +0100

    fix syntax meta missing

--- src/metadata/MetadataContainer.ts
+++ src/metadata/MetadataContainer.ts
@@ -2,32 +2,32 @@ import { PreconditionMeta } from "./PreconditionMeta";
 import { SyntaxMeta } from "./SyntaxMeta";
 
 class MetadataContainer {
-    readonly commandPreconditions: PreconditionMeta[] = [];
-    readonly commandSyntaxes: SyntaxMeta[] = [];
+	readonly commandPreconditions: PreconditionMeta[] = [];
+	readonly commandSyntaxes: SyntaxMeta[] = [];
 
-    filterPreconditions(target: Function | string): PreconditionMeta[];
-    filterPreconditions(target: (Function | string)[]): PreconditionMeta[];
-    filterPreconditions(target: (Function | string) | (Function | string)[]): PreconditionMeta[] {
-        return this.filterByTarget(this.commandPreconditions, target);
-    }
+	filterPreconditions(target: Function | string): PreconditionMeta[];
+	filterPreconditions(target: (Function | string)[]): PreconditionMeta[];
+	filterPreconditions(target: (Function | string) | (Function | string)[]): PreconditionMeta[] {
+		return this.filterByTarget(this.commandPreconditions, target);
+	}
 
-    filterSyntax(target: Function | string): SyntaxMeta[];
-    filterSyntax(target: (Function | string)[]): SyntaxMeta[];
-    filterSyntax(target: (Function | string) | (Function | string)[]): SyntaxMeta[] {
-        return this.filterByTarget(this.commandSyntaxes, target);
-    }
+	filterSyntax(target: Function | string): SyntaxMeta[];
+	filterSyntax(target: (Function | string)[]): SyntaxMeta[];
+	filterSyntax(target: (Function | string) | (Function | string)[]): SyntaxMeta[] {
+		return this.filterByTarget(this.commandSyntaxes, target);
+	}
 
-    /**
-     * Filters given array by a given target or targets.
-     */
-    protected filterByTarget<T extends { target: Function | string }>(
-        array: T[],
-        target: (Function | string) | (Function | string)[]
-    ): T[] {
-        return array.filter((val) => {
-            return Array.isArray(target) ? target.indexOf(val.target) !== -1 : val.target === target;
-        });
-    }
+	/**
+	 * Filters given array by a given target or targets.
+	 */
+	protected filterByTarget<T extends { target: Function | string }>(
+		array: T[],
+		target: (Function | string) | (Function | string)[]
+	): T[] {
+		return array.filter((val) => {
+			return Array.isArray(target) ? target.indexOf(val.target) !== -1 : val.target === target;
+		});
+	}
 }
 
 export const container = new MetadataContainer();
--- src/metadata/SyntaxMeta.ts
+++ src/metadata/SyntaxMeta.ts
@@ -1,14 +1,14 @@
 import { Context } from "../structures/Context";
 
-export interface ModuleCommandMeta {
-    target: Function;
-    value: (context: Context) => any;
+export interface SyntaxMeta {
+	target: Function;
+	value: (context: Context) => any;
 }
 
 /**
  * Represents a module command.
  * @returns
  */
-export const ModuleCommand = (): MethodDecorator => {
-    return (target, key, descriptor) => {};
+export const Syntax = (): MethodDecorator => {
+	return (target, key, descriptor) => {};
 };
