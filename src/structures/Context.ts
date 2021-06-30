2dbad925c11cd0b31a1392a5f5ed59d86dd9d2db
SkyezerFox <actuallyori@gmail.com>
2021-06-30 11:42:55 +0100

    fix getters in Context

--- src/structures/Context.ts
+++ src/structures/Context.ts
@@ -8,47 +8,39 @@ import { ContextEmbed } from "./ContextEmbed";
  * Represents the context in which a command was run.
  */
 export class Context {
-	constructor(message: Message) {
-		this._message = message;
-	}
-
-	// workaround for needing to specify types on cloned methods
-	private _message: Message;
-
-	/**
-	 * The message this content represents.
-	 */
-	get message(): Message {
-		return this._message;
-	}
+	constructor(readonly message: Message) {}
 
 	/**
 	 * The user who ran the command.
 	 */
 	get author() {
-		return this._message.author;
+		return this.message.author;
 	}
 
 	/**
 	 * The channel the command was executed in.
 	 */
 	get channel() {
-		return this._message.channel;
+		return this.message.channel;
 	}
 
 	get guild(): Guild | null {
-		return this._message.guild;
+		return this.message.guild;
 	}
 
 	/**
 	 * Reply to this message.
 	 */
-	reply = this.message.reply;
+	get reply() {
+		return this.message.reply;
+	}
 
 	/**
 	 * Send a message to the channel this context was created in.
 	 */
-	send = this.channel.send.bind(this.channel);
+	get send() {
+		return this.channel.send.bind(this.channel);
+	}
 
 	/**
 	 * Create a new context embed.
